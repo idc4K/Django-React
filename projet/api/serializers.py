32fbee13a350a41baa8d97b9d0c668417659c060
christ <christikonga83@gmail.com>
2022-04-05 00:34:45 +0200

    all

--- projet/api/serializers.py
+++ projet/api/serializers.py
@@ -1,4 +1,4 @@
-from django.core import serializers
+from rest_framework import serializers
 from .models import Table
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
--- projet/api/urls.py
+++ projet/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.api_home),
+    path('', views.TodoListApiView),
     
 ]
