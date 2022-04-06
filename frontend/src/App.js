f1c358b20e0c7f69c5891f6b3864e7404bdb98e8
christ <christikonga83@gmail.com>
2022-04-06 13:25:00 +0200

    all

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
 
 import './App.css';
-import React from 'react'
+import React from 'react';
 
 
-class App extends React.Components{
+class App extends React.Component {
     render(){
       return(
         <div className='conatiner'>
+            <div className='task-container'>
+               <div className='form-wrapper'>
+                    <form id="form">
+                       <div className='flex-wrapper'>
+                          <div style={{flex:6}}>
+                              <input  className='form-control' id="nom" type="text" placeholder='nom' name='nom'/>
+                              <input  className='form-control' id="nom" type="text" placeholder='nom' name='nom'/>
+                          </div>
 
+                          <div style={{flex:1}}>
+                              <input id="submit" className='btn btn-danger'  type="submit" name='Ajouter'/>
+                          </div>
+                       </div>
+                    </form>
+                    <div id='list-wrapper'>
+
+                    </div>
+               </div>
+            </div>
         </div>
       )
     }
